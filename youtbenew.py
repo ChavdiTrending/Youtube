4dc5addc894effd351213a5fdc72110d1b65e4cd
savan_nahar68 <savannahar68@gmail.com>
2018-04-14 20:50:46 +0530

    update final

---
+++ youtbenew.py
@@ -0,0 +1,20 @@
+import requests
+import json
+cnt = 0
+nextpage = ''
+links = []
+while cnt<2:
+    payload = {'part': 'snippet', 'key': 'AIzaSyA46dBqdBBtnfgxr8aRvSBE2Q7qiNyhWrk', 'maxResults': '50','chart':'mostPopular','regionCode':'IN','pageToken':nextpage}
+    l = requests.Session().get('https://www.googleapis.com/youtube/v3/videos', params=payload)
+    resp_dict = json.loads(l.content)
+    print(resp_dict)
+    nextpage = resp_dict['nextPageToken']
+    for i in resp_dict['items']:
+        links.append("https://youtu.be/"+i['id'])
+        links.append(i['snippet']['title'])
+        links.append(i['snippet']['thumbnails']['high']['url'])
+    cnt+=1
+jsonDict = {}
+jsonDict['links'] = links
+with open('Videolinks.json', 'w') as outfile:
+    json.dump(jsonDict, outfile)
