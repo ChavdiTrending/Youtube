c3d3163cd1c72a48141569b13a82def0ecf1d6f3
savan_nahar68 <savannahar68@gmail.com>
2018-04-14 20:35:58 +0530

    Updated Script gives title and content too

--- youtubelinks.py
+++ youtubelinks.py
@@ -4,15 +4,20 @@ cnt = 0
 nextpage = ''
 links = []
 while cnt<2:
-	payload = {'part': 'id', 'key': 'AIzaSyA46dBqdBBtnfgxr8aRvSBE2Q7qiNyhWrk', 'maxResults': '50','chart':'mostPopular','regionCode':'IN','pageToken':nextpage}
-	l = requests.Session().get('https://www.googleapis.com/youtube/v3/videos', params=payload)    
+	payload = {'part': 'snippet', 'key': 'AIzaSyA46dBqdBBtnfgxr8aRvSBE2Q7qiNyhWrk', 'maxResults': '50','chart':'mostPopular','regionCode':'IN','pageToken':nextpage}
+	l = requests.Session().get('https://www.googleapis.com/youtube/v3/videos', params=payload)
 	resp_dict = json.loads(l.content)
 	print(resp_dict)
+	with open('Videolinks.json', 'w') as outfile:
+	    json.dump(resp_dict, outfile)
+	break
+	'''
 	nextpage = resp_dict['nextPageToken']
 	for i in resp_dict['items']:
 	     links.append("https://youtu.be/"+i['id'])
 	cnt+=1
 jsonDict = {}
 jsonDict['links'] = links
-with open('Videolinks.json', 'w') as outfile:  
+with open('Videolinks.json', 'w') as outfile:
     json.dump(jsonDict, outfile)
+'''
